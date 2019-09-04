00e4931bffa37670d489b75a8395a994f9cd7781
ch.mellado@inmade.cl <ch.mellado@inmade.cl>
2019-09-03 20:36:44 -0400

    Primera Commit en Prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ $(function(){
         event.preventDefault();
 
         var cat_ = this.hash;
-        $("html, body").animate({scrollTop: $(cat_).offset().top}, 500, function(){
+        $("html, body").animate({scrollTop: $(cat_).offset().top}, 900, function(){
           window.location.hash = cat_;
         });
        }
    });
-     // $('[data-toggle="popover"]').popover();
+      $('[data-toggle="popover"]').popover();
 });
